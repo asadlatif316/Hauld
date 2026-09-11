7fb56da32e85362985e183ee5cc457c99d5f171b
Asad <hellohello51981@gmail.com>
2026-09-11 15:47:59 +0500

    feat: basic server setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,29 @@
-console.log(
-    'server is working.....'
+import express from 'express';
+import dotenv from 'dotenv';
+import statusCode from 'http-status-codes';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
 );
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res
+    .status(statusCode.OK)
+    .json({ success: true, message: `Server is running on port: ${port}` });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
