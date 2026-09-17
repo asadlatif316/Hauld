678014afea30f1da55b7931738471e5829439d0c
Asad <hellohello51981@gmail.com>
2026-09-17 12:20:01 +0500

    feat: add checkAuth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes';
 import { ToastWrapper } from './components';
-
+import { useAuthStore } from './store';
+import { useEffect } from 'react';
 const router = createBrowserRouter([...routes]);
 
 function App() {
+  const { checkAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
   return (
     <>
       <RouterProvider router={router} />
--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -6,10 +6,22 @@ const useAuthStore = create((set) => ({
   isLoggingIn: false,
   isCheckingAuth: false,
 
+  checkAuth: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const res = await api.get('/auth/user/me');
+      set({ user: res.data });
+    } catch (error) {
+      set({ user: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
   login: async (data) => {
     set({ isLoggingIn: true });
     try {
-      const res = await api.post('/auth/login',data);
+      const res = await api.post('/auth/login', data);
       set({ user: res.data });
     } catch (error) {
       console.log(error);
