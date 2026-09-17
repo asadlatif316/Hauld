22fba5e33c8d03d05ee79bd7f56c03ec72506c5e
Asad <hellohello51981@gmail.com>
2026-09-17 23:50:51 +0500

    feat: complete nav Items styling

--- client/src/components/admin/adminNavbar.jsx
+++ client/src/components/admin/adminNavbar.jsx
@@ -1,17 +1,30 @@
-import { navItems } from "@/config/nav"
-import { NavLink } from "react-router-dom"
+import { navItems } from '@/config/nav';
+import { NavLink } from 'react-router-dom';
+import { cn } from '@/lib/utils';
 
 const AdminNavbar = () => {
   return (
-    <div className="text-sidebar-foreground">
-          {navItems.map((item) => (
-              <NavLink to={item.to} key={item.to} end={item.end}>
-                  <item.icon />
-                {item.label}
-            </NavLink>
-        ))}
-      </div>
-  )
-}
+    <div className='text-sidebar-foreground flex flex-col space-y-3 p-4'>
+      {navItems.map((item) => (
+        <NavLink
+          to={item.to}
+          key={item.to}
+          end={item.end}
+          className={({ isActive }) =>
+            cn(
+              'flex items-center gap-x-2 rounded-md text-sm px-3 py-2 font-medium transition-color duration-200',
+              isActive
+                ? 'bg-sidebar-accent text-primary-foreground'
+                : 'text-muted-foreground hover:text-sidebar-foreground hover:bg-accent-foreground',
+            )
+          }
+        >
+          <item.icon className='size-5 shrink-0' />
+          {item.label}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
 
-export default AdminNavbar
+export default AdminNavbar;
--- client/src/config/nav.js
+++ client/src/config/nav.js
@@ -7,9 +7,9 @@ import {
 } from 'lucide-react';
 
 export const navItems = [
-  { to: '/admin', label: 'Dashboard', icon: LayoutDashboard, end: true },
-  { to: '/admin/products', label: 'Products', icon: Package },
-  { to: '/admin/orders', label: 'Orders', icon: ShoppingCart },
-  { to: '/admin/messages', label: 'Messages', icon: MessageSquare },
-  { to: '/admin/settings', label: 'Settings', icon: Settings },
+  { to: '/dashboard', label: 'Dashboard', icon: LayoutDashboard, end: true },
+  { to: '/dashboard/products', label: 'Products', icon: Package },
+  { to: '/dashboard/orders', label: 'Orders', icon: ShoppingCart },
+  { to: '/dashboard/messages', label: 'Messages', icon: MessageSquare },
+  { to: '/dashboard/settings', label: 'Settings', icon: Settings },
 ];
