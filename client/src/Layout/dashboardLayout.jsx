6d20e847c7c38a54f78e252b9e46dda31aacfe5e
Asad <hellohello51981@gmail.com>
2026-09-18 12:14:44 +0500

    feat: set Scrim for Admin sidebar

--- client/src/Layout/dashboardLayout.jsx
+++ client/src/Layout/dashboardLayout.jsx
@@ -1,10 +1,15 @@
 import { AdminHeader, AdminSidebar } from '@/components';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 const DashboardLayout = () => {
+  const [isOpen,setIsOpen] = useState(true)
   return (
     <div className='relative h-screen'>
       <AdminSidebar />
-      <div className='ml-72'>
+
+      {isOpen && <div className='fixed inset-0 bg-primary/50 md:hidden'>
+        </div>}
+      <div className='md:ml-72'>
         <AdminHeader />
         <main>
           <Outlet />
--- client/src/components/admin/adminSidebar.jsx
+++ client/src/components/admin/adminSidebar.jsx
@@ -2,7 +2,7 @@ import AdminNavbar from './adminNavbar';
 
 const AdminSidebar = () => {
   return (
-    <div className='fixed left-0 top-0 w-full h-screen md:max-w-72 flex flex-col p-2'>
+    <div className='fixed left-0 top-0 h-screen z-50 w-72 flex flex-col p-2'>
       <div className='py-2 bg-sidebar h-full flex flex-col space-y-2 rounded-lg'>
         <div className='flex h-14 items-center text-sidebar-foreground px-5'>
           <span className='text-2xl font-semibold tracking-tight'>Hauld</span>
