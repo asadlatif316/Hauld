216154124fac469db1f4ae9a068a3ff0fbbc007c
Asad <hellohello51981@gmail.com>
2026-09-18 15:14:12 +0500

    feat: add Sidebar close state and functionality

--- client/src/Layout/dashboardLayout.jsx
+++ client/src/Layout/dashboardLayout.jsx
@@ -2,15 +2,13 @@ import { AdminHeader, AdminSidebar } from '@/components';
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 const DashboardLayout = () => {
-  const [isOpen,setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <div className='relative h-screen'>
-      <AdminSidebar />
-
-      {isOpen && <div className='fixed inset-0 bg-primary/50 md:hidden'>
-        </div>}
+      <AdminSidebar setIsOpen={setIsOpen} isOpen={isOpen} />
+      {isOpen && <div className='fixed inset-0 bg-primary/50 md:hidden'></div>}
       <div className='md:ml-72'>
-        <AdminHeader />
+        <AdminHeader setIsOpen={setIsOpen} />
         <main>
           <Outlet />
         </main>
--- client/src/components/admin/adminSidebar.jsx
+++ client/src/components/admin/adminSidebar.jsx
@@ -1,11 +1,27 @@
 import AdminNavbar from './adminNavbar';
+import { X, PanelLeft, PanelLeftClose } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
-const AdminSidebar = () => {
+const AdminSidebar = ({ setIsOpen, isOpen }) => {
   return (
-    <div className='fixed left-0 top-0 h-screen z-50 w-72 flex flex-col p-2'>
-      <div className='py-2 bg-sidebar h-full flex flex-col space-y-2 rounded-lg'>
-        <div className='flex h-14 items-center text-sidebar-foreground px-5'>
+    <div
+      className={cn(
+        'fixed left-0 top-0 h-screen z-50 w-72 flex flex-col p-2 transition-transform ease-in-out duration-300 md:translate-x-0',
+        isOpen ? 'translate-x-0' : '-translate-x-full',
+      )}
+    >
+      <div className='relative py-2 bg-sidebar h-full flex flex-col space-y-2 rounded-lg'>
+        <div className='flex justify-between h-14 items-center text-sidebar-foreground px-5'>
           <span className='text-2xl font-semibold tracking-tight'>Hauld</span>
+
+          <button
+            aria-label='Close menu'
+            className='md:hidden cursor-pointer group relative size-11'
+            onClick={() => setIsOpen(false)}
+          >
+            <PanelLeft className='absolute inset-0 m-auto group-hover:opacity-0 transition-opacity' />
+            <PanelLeftClose className='absolute inset-0 m-auto opacity-0 group-hover:opacity-100 transition-opacity' />
+          </button>
         </div>
         <AdminNavbar />
       </div>
