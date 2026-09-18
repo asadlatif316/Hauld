45816777ee583258a1713983f9c3c634278fad0e
Asad <hellohello51981@gmail.com>
2026-09-18 15:41:48 +0500

    feat: add sidebar open state and functionality

--- client/src/components/admin/adminHeader.jsx
+++ client/src/components/admin/adminHeader.jsx
@@ -1,9 +1,24 @@
-const AdminHeader = () => {
+import {  HiMenuAlt1, HiMenuAlt2 } from 'react-icons/hi';
+
+const AdminHeader = ({setIsOpen}) => {
   return (
     <div>
-      AdminHeader
+      <button
+        aria-label='Open menu'
+        onClick={() => setIsOpen(true)}
+        className='group relative size-13 md:hidden cursor-pointer'
+      >
+        <HiMenuAlt1
+          className='absolute inset-0 m-auto transition-transform duration-200 group-hover:scale-0'
+          size={26}
+        />
+        <HiMenuAlt2
+          className='absolute inset-0 m-auto scale-0 transition-transform duration-200 group-hover:scale-100'
+          size={26}
+        />
+      </button>
     </div>
-  )
+  );
 }
 
 export default AdminHeader
