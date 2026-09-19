c0ec1280bda50f67951b691493759a2f4863fb2c
Asad <hellohello51981@gmail.com>
2026-09-19 23:31:49 +0500

    feat: add logout functionality

--- client/src/components/UserMenu/index.jsx
+++ client/src/components/UserMenu/index.jsx
@@ -1,20 +1,27 @@
-import { DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuTrigger } from "@/lib"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/lib';
+import { useAuthStore } from '@/store';
 import { BiLogOut } from 'react-icons/bi';
 
 const UserMenu = () => {
+  const { logout } = useAuthStore();
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className='cursor-pointer'>
         <div className='h-10 w-10 bg-primary rounded-full'></div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='w-40 min-w-0'>
-        <DropdownMenuItem className='cursor-pointer'>
-          <BiLogOut className="size-4"/>
+        <DropdownMenuItem className='cursor-pointer' onClick={logout}>
+          <BiLogOut className='size-4' />
           <span>Logout</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
 
-export default UserMenu
+export default UserMenu;
