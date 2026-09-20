b3614a399cb6b0633dbb0895a3d5d528146b8aac
Asad <hellohello51981@gmail.com>
2026-09-20 11:04:35 +0500

    feat: product model index and update product model

--- server/src/models/product.model.js
+++ server/src/models/product.model.js
@@ -1,19 +1,28 @@
 import mongoose from 'mongoose';
 
-const ProductSchema = mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  slug: { type: String, required: true },
-  description: { type: String },
-  price: { type: Number, required: true },
-  category: {
-    enum: ['backpack', 'tote', 'duffel', 'crossBody', 'wallet', 'laptop-bag'],
-    required: true,
+const ProductSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    slug: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    category: {
+      enum: ['backpack', 'tote', 'duffel', 'crossBody', 'wallet', 'laptop-bag'],
+      required: true,
+    },
+    image: {
+      url: { type: String, required: true },
+      publicId: { type: String, required: true },
+    },
+    isFeatured: { type: Boolean, required: true },
+    isActive: { type: Boolean, required: true },
+    stock: { type: Number, required: true },
   },
-  imageURL: { type: String, required: true },
-  isFeatured: { type: Boolean, required: true },
-  isActive: { type: Boolean, required: true },
-  stock: { type: Number, required: true },
-});
+  { timestamps: true },
+);
 
-const Product = mongoose.model.Product || mongoose.model('Product', ProductSchema);
+ProductSchema.index({ name: 'text' });
+ProductSchema.index({ category: 1, createdAt: -1 });
+
+const Product = mongoose.model('Product', ProductSchema);
 export default Product;
