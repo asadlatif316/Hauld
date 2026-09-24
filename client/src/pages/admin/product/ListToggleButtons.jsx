ef164b8b4b2fc647e370f8e0a2047d47f3cc22f7
Asad <hellohello51981@gmail.com>
2026-09-24 21:15:29 +0500

    feat: complete toggle view buttons

--- client/src/pages/admin/product/ListToggleButtons.jsx
+++ client/src/pages/admin/product/ListToggleButtons.jsx
@@ -1,23 +1,40 @@
-import {cn} from '@/lib/utils'
+import { cn } from '@/lib/utils';
 import { TbLayoutList, TbLayoutGrid } from 'react-icons/tb';
 import { Button } from '@/components';
 import { useState } from 'react';
 
 const ListToggleButtons = () => {
-    const [view,setView] = useState('list')
+  const [view, setView] = useState('list');
   return (
     <div>
       <div className='inline-flex bg-muted rounded-full  shadow-lg'>
         <Button
-          icon={
-            <TbLayoutList className='text-primary group-hover:scale-[1.05]' />
-          }
-          className='group rounded-full px-2 bg-transparent border-none hover:bg-transparent '
+          aria-label='List View'
+          aria-pressed={view === 'list'}
+          onClick={() => setView('list')}
+          icon={<TbLayoutList className='group-hover:scale-[1.05] size-4' />}
+          className={cn(
+            'group rounded-full border-transparent px-2 ',
+            view === 'list'
+              ? 'bg-primary text-primary-foreground '
+              : 'hover:bg-transparent bg-transparent text-primary',
+          )}
         />
-        <Button icon={<TbLayoutGrid />} className='rounded-full px-2' />
+        <Button
+          aria-label='Grid View'
+          aria-pressed={view === 'grid'}
+          onClick={() => setView('grid')}
+          icon={<TbLayoutGrid className='group-hover:scale-[1.05] size-4' />}
+          className={cn(
+            'group rounded-full border-transparent px-2 ',
+            view === 'grid'
+              ? 'bg-primary text-primary-foreground '
+              : 'hover:bg-transparent bg-transparent text-primary',
+          )}
+        />
       </div>
     </div>
   );
-}
+};
 
-export default ListToggleButtons
+export default ListToggleButtons;
