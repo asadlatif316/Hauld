b67764b2e698fe430122e1e36cd574880bc1726f
Asad <hellohello51981@gmail.com>
2026-09-24 15:44:30 +0500

    feat: added select component in toolbar

--- client/src/pages/admin/product/productToolbar.jsx
+++ client/src/pages/admin/product/productToolbar.jsx
@@ -4,10 +4,11 @@ import { TbLayoutList, TbLayoutGrid } from 'react-icons/tb';
 import { IoMdAdd } from 'react-icons/io';
 import { Funnel } from 'lucide-react';
 import { useState } from 'react';
-import {  SORT_OPTIONS } from '@/config';
+import { FEATURED_OPTIONS, SORT_OPTIONS } from '@/config';
 
 const ProductToolbar = () => {
   const [sortBy, setSortBy] = useState('-createdAt');
+  const [featured, setFeatured] = useState('All');
   return (
     <div>
       <div>
@@ -17,19 +18,28 @@ const ProductToolbar = () => {
         </div>
         <form>
           <FormInput type='text' />
-          <FormSelect value={sortBy} options={SORT_OPTIONS} onChange={setSortBy} label='Sort by'/>
-          <select name='' id=''>
-            <option value=''>show</option>
-            <option value=''>All</option>
-            <option value=''>Active</option>
-            <option value=''>Inactive</option>
-          </select>
+          <FormSelect
+            value={featured}
+            options={FEATURED_OPTIONS}
+            onChange={setFeatured}
+            label='Show'
+          />
+          <FormSelect
+            value={sortBy}
+            options={SORT_OPTIONS}
+            onChange={setSortBy}
+            label='Sort by'
+          />
           <Button
             label='Filter'
             iconDirection='left'
             icon={<Funnel size={17} />}
           />
-          <Button label='Add new Product' iconDirection='left' icon={<IoMdAdd size={20}/>} />
+          <Button
+            label='Add new Product'
+            iconDirection='left'
+            icon={<IoMdAdd size={20} />}
+          />
         </form>
       </div>
       <ProductFilters />
