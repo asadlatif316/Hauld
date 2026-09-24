6ab1f3f1dbb7a7b33742e7fdf41a4c5014920e1f
Asad <hellohello51981@gmail.com>
2026-09-24 11:03:26 +0500

    feat: added button styling for icon

--- client/src/components/button/index.jsx
+++ client/src/components/button/index.jsx
@@ -1,9 +1,18 @@
 import { cn } from '@/lib/utils';
 
-const Button = ({ children, className, type = 'button', ...props }) => {
+const Button = ({
+  label,
+  className,
+  icon,
+  type = 'button',
+  ref,
+  iconDirection = 'right',
+  ...props
+}) => {
   return (
     <div className='w-full sm:w-auto'>
       <button
+        ref={ref}
         type={type}
         className={cn(
           'flex items-center justify-center gap-2 whitespace-nowrap cursor-pointer px-4 py-2 font-semibold rounded-xl bg-primary text-primary-foreground hover:bg-primary/90 transition-all duration-300 active:scale-[0.96] border',
@@ -11,7 +20,19 @@ const Button = ({ children, className, type = 'button', ...props }) => {
         )}
         {...props}
       >
-        {children}
+        <span className='relative flex items-center gap-2 z-10'>
+          {iconDirection === 'left' ? (
+            <>
+              {icon && icon}
+              {label}
+            </>
+          ) : (
+            <>
+              {label}
+              {icon && icon}
+            </>
+          )}
+        </span>
       </button>
     </div>
   );
