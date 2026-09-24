52e3a1eda1172d21b99a49750b378f220e621a76
Asad <hellohello51981@gmail.com>
2026-09-24 21:18:27 +0500

    FEAT: SHOW BUTTONS ON DESKTOP ONLY

--- client/src/pages/admin/product/ListToggleButtons.jsx
+++ client/src/pages/admin/product/ListToggleButtons.jsx
@@ -6,33 +6,31 @@ import { useState } from 'react';
 const ListToggleButtons = () => {
   const [view, setView] = useState('list');
   return (
-    <div>
-      <div className='inline-flex bg-muted rounded-full  shadow-lg'>
-        <Button
-          aria-label='List View'
-          aria-pressed={view === 'list'}
-          onClick={() => setView('list')}
-          icon={<TbLayoutList className='group-hover:scale-[1.05] size-4' />}
-          className={cn(
-            'group rounded-full border-transparent px-2 ',
-            view === 'list'
-              ? 'bg-primary text-primary-foreground '
-              : 'hover:bg-transparent bg-transparent text-primary',
-          )}
-        />
-        <Button
-          aria-label='Grid View'
-          aria-pressed={view === 'grid'}
-          onClick={() => setView('grid')}
-          icon={<TbLayoutGrid className='group-hover:scale-[1.05] size-4' />}
-          className={cn(
-            'group rounded-full border-transparent px-2 ',
-            view === 'grid'
-              ? 'bg-primary text-primary-foreground '
-              : 'hover:bg-transparent bg-transparent text-primary',
-          )}
-        />
-      </div>
+    <div className='hidden sm:inline-flex bg-muted rounded-full shadow-lg'>
+      <Button
+        aria-label='List View'
+        aria-pressed={view === 'list'}
+        onClick={() => setView('list')}
+        icon={<TbLayoutList className='group-hover:scale-[1.05] size-4' />}
+        className={cn(
+          'group rounded-full border-transparent px-2 ',
+          view === 'list'
+            ? 'bg-primary text-primary-foreground '
+            : 'hover:bg-transparent bg-transparent text-primary',
+        )}
+      />
+      <Button
+        aria-label='Grid View'
+        aria-pressed={view === 'grid'}
+        onClick={() => setView('grid')}
+        icon={<TbLayoutGrid className='group-hover:scale-[1.05] size-4' />}
+        className={cn(
+          'group rounded-full border-transparent px-2 ',
+          view === 'grid'
+            ? 'bg-primary text-primary-foreground '
+            : 'hover:bg-transparent bg-transparent text-primary',
+        )}
+      />
     </div>
   );
 };
